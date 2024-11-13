ab96c2f1e9245138ec6d280e4c7db659a333bd50
AnsaryTanvir <109978583+AnsaryTanvir@users.noreply.github.com>
2024-11-13 16:55:50 +0600

    Update syscallhook.c

--- kpms/demo-syscallhook/syscallhook.c
+++ kpms/demo-syscallhook/syscallhook.c
@@ -21,52 +21,45 @@ KPM_DESCRIPTION("KernelPatch Module System Call Hook Example");
 const char *margs = 0;
 enum hook_type hook_type = NONE;
 
-enum pid_type
+void before_mincore_0(hook_fargs4_t *args, void *udata)
 {
-    PIDTYPE_PID,
-    PIDTYPE_TGID,
-    PIDTYPE_PGID,
-    PIDTYPE_SID,
-    PIDTYPE_MAX,
-};
-struct pid_namespace;
-pid_t (*__task_pid_nr_ns)(struct task_struct *task, enum pid_type type, struct pid_namespace *ns) = 0;
+    unsigned long start = (unsigned long)syscall_argn(args, 0);
+    size_t len = (size_t)syscall_argn(args, 1);
+    unsigned char __user *vec = (unsigned char __user *)syscall_argn(args, 2);
 
-void before_openat_0(hook_fargs4_t *args, void *udata)
-{
-    int dfd = (int)syscall_argn(args, 0);
-    const char __user *filename = (typeof(filename))syscall_argn(args, 1);
-    int flag = (int)syscall_argn(args, 2);
-    umode_t mode = (int)syscall_argn(args, 3);
+    pr_info("mincore syscall hooked: returning fake response\n");
 
-    char buf[1024];
-    compat_strncpy_from_user(buf, filename, sizeof(buf));
+    /* Populate the vec buffer with zeros to simulate unmapped pages */
+    if (vec && len > 0) {
+        size_t page_count = (len + PAGE_SIZE - 1) / PAGE_SIZE; // Round up to page count
+        unsigned char *fake_vec = kzalloc(page_count, GFP_KERNEL); // Allocate and zero a fake vector
 
-    struct task_struct *task = current;
-    pid_t pid = -1, tgid = -1;
-    if (__task_pid_nr_ns) {
-        pid = __task_pid_nr_ns(task, PIDTYPE_PID, 0);
-        tgid = __task_pid_nr_ns(task, PIDTYPE_TGID, 0);
+        if (fake_vec) {
+            if (copy_to_user(vec, fake_vec, page_count)) {
+                pr_warn("mincore hook: Failed to copy fake vec to user\n");
+            }
+            kfree(fake_vec);
+        }
     }
 
-    args->local.data0 = (uint64_t)task;
-
-    pr_info("hook_chain_0 task: %llx, pid: %d, tgid: %d, openat dfd: %d, filename: %s, flag: %x, mode: %d\n", task, pid,
-            tgid, dfd, buf, flag, mode);
+    /* Set return value to 0, simulating successful execution with all pages as unmapped */
+    args->ret = 0;
+    args->done = 1;  // Skip the actual mincore call
 }
 
-uint64_t open_counts = 0;
+uint64_t mincore_counts = 0;
 
-void before_openat_1(hook_fargs4_t *args, void *udata)
+void before_mincore_1(hook_fargs4_t *args, void *udata)
 {
+    /* Count the occurrences if needed */
     uint64_t *pcount = (uint64_t *)udata;
     (*pcount)++;
-    pr_info("hook_chain_1 before openat task: %llx, count: %llx\n", args->local.data0, *pcount);
+    pr_info("hook_chain_1 before mincore task: %llx, count: %llx\n", args->local.data0, *pcount);
 }
 
-void after_openat_1(hook_fargs4_t *args, void *udata)
+void after_mincore_1(hook_fargs4_t *args, void *udata)
 {
-    pr_info("hook_chain_1 after openat task: %llx\n", args->local.data0);
+    pr_info("hook_chain_1 after mincore task: %llx\n", args->local.data0);
 }
 
 static long syscall_hook_demo_init(const char *args, const char *event, void *__user reserved)
@@ -74,26 +67,23 @@ static long syscall_hook_demo_init(const char *args, const char *event, void *__
     margs = args;
     pr_info("kpm-syscall-hook-demo init ..., args: %s\n", margs);
 
-    __task_pid_nr_ns = (typeof(__task_pid_nr_ns))kallsyms_lookup_name("__task_pid_nr_ns");
-    pr_info("kernel function __task_pid_nr_ns addr: %llx\n", __task_pid_nr_ns);
+    hook_err_t err = HOOK_NO_ERR;
 
     if (!margs) {
         pr_warn("no args specified, skip hook\n");
         return 0;
     }
 
-    hook_err_t err = HOOK_NO_ERR;
-
     if (!strcmp("function_pointer_hook", margs)) {
         pr_info("function pointer hook ...");
         hook_type = FUNCTION_POINTER_CHAIN;
-        err = fp_hook_syscalln(__NR_openat, 4, before_openat_0, 0, 0);
+        err = fp_hook_syscalln(__NR_mincore, 3, before_mincore_0, 0, 0);
         if (err) goto out;
-        err = fp_hook_syscalln(__NR_openat, 4, before_openat_1, after_openat_1, &open_counts);
+        err = fp_hook_syscalln(__NR_mincore, 3, before_mincore_1, after_mincore_1, &mincore_counts);
     } else if (!strcmp("inline_hook", margs)) {
         pr_info("inline hook ...");
         hook_type = INLINE_CHAIN;
-        err = inline_hook_syscalln(__NR_openat, 4, before_openat_0, 0, 0);
+        err = inline_hook_syscalln(__NR_mincore, 3, before_mincore_0, 0, 0);
     } else {
         pr_warn("unknown args: %s\n", margs);
         return 0;
@@ -101,9 +91,9 @@ static long syscall_hook_demo_init(const char *args, const char *event, void *__
 
 out:
     if (err) {
-        pr_err("hook openat error: %d\n", err);
+        pr_err("hook mincore error: %d\n", err);
     } else {
-        pr_info("hook openat success\n");
+        pr_info("hook mincore success\n");
     }
     return 0;
 }
@@ -119,11 +109,10 @@ static long syscall_hook_demo_exit(void *__user reserved)
     pr_info("kpm-syscall-hook-demo exit ...\n");
 
     if (hook_type == INLINE_CHAIN) {
-        inline_unhook_syscall(__NR_openat, before_openat_0, 0);
+        inline_unhook_syscall(__NR_mincore, before_mincore_0, 0);
     } else if (hook_type == FUNCTION_POINTER_CHAIN) {
-        fp_unhook_syscall(__NR_openat, before_openat_0, 0);
-        fp_unhook_syscall(__NR_openat, before_openat_1, after_openat_1);
-    } else {
+        fp_unhook_syscall(__NR_mincore, before_mincore_0, 0);
+        fp_unhook_syscall(__NR_mincore, before_mincore_1, after_mincore_1);
     }
     return 0;
 }
